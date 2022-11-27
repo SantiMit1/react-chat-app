fc284a7c1455118392014c182f028f1314734061
SantiMit1 <santimiti10@gmail.com>
2022-11-27 20:11:47 -0300

    primer commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-5ImO64Hg_qaYG6oj10MvbRDgu58MBdQ",
+  authDomain: "chat-app-6f452.firebaseapp.com",
+  projectId: "chat-app-6f452",
+  storageBucket: "chat-app-6f452.appspot.com",
+  messagingSenderId: "488683405961",
+  appId: "1:488683405961:web:2a0c4ff04387e2f7a5fc58"
+};
+
+const app = initializeApp(firebaseConfig);
